23382789d9d48cb8e59c41ddde48189606ca6066
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-28 20:58:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,5 +12,5 @@ lit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-lit.multiselect('Pick some fruits: ', list(my_druit_list.index))
+lit.multiselect('Pick some fruits: ', list(my_fruit_list.index))
 lit.dataframe(my_fruit_list)
