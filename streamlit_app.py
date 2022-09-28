886ebbec7a5a16a073df0449fc50bed5a519bb22
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-28 20:17:30 +0200

    Update streamlit_app.py
    added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
-import streamlit as l
+import streamlit as lit
 
-l.title('MY')
+lit.title('My parents new Healthy Menu')
+lit.header('Breakfast Favorites')
+lit.text('🥣 Omega 3 & Blueberry Oatmeal')
+lit.text('🥗 Kale, Spinach & Rocket Smoothie')
+lit.text('🐔 Hard-Boiled Free-Range Egg') 
+lit.text('🥑🍞 Avocado Toast')   
+lit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
