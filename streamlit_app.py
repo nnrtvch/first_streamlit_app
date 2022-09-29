0f73a867dba7333594a558e2e2c6619e12eace95
nnrtvch <56116379+nnrtvch@users.noreply.github.com>
2022-09-29 16:37:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,4 @@ lit.dataframe(fruits_to_show)
 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+lit.text(fruityvice_response)
